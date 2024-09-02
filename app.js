31bb132cd9d1310ded646b1b06acdd44685188e5
unknown <fareha0402@gmail.com>
2024-09-02 16:38:44 +0530

    Ch-3 : babel use and jsx

--- app.js
+++ app.js
@@ -1,47 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement('h1',{},'Hello World from React') 
-const headingWithAttr = React.createElement('h1',{id:'heading',},'Hello World from React')
- console.log(heading) // Object
-
-Eg:
-{/* <div id="parent">
-    <div id="child">
-        <h1>I'm a h1 tag</h1>
-     </div>   
-</div> */}
- const parent = React.createElement('div',{id:'parent',},React.createElement('div',{id:'child',},React.createElement('h1',{},"I'm a h1 tag")))
- console.log(parent) //
-//  const root = ReactDOM.createRoot(document.getElementById('root'))
-//  root.render(parent) // is Object React.creatElement is an Object ==> becomes a HTML that Browser understands while rendering to dom --> conversion happens so Object gets converted
- Eg: 
-{/* <div id="parent">
-    <div id="child">
-        <h1>I'm a h1 tag</h1>
-        <h2>I'm a h2 tag</h1>
-     </div>   
-</div> */}
-// type of element,{},children(array)
-const siblings = React.createElement('div',{id:'parent'},
-    [React.createElement('div',{id:'child'},
-    [React.createElement('h1',{},"I'm a h1 tag"),
-     React.createElement('h2',{},"I'm a h2 tag")]
-    )])
- console.log(siblings) //
-//  const root = ReactDOM.createRoot(document.getElementById('root'))
-//  root.render(siblings)
- const children = React.createElement('div',{id:'parent'},[
-    React.createElement('div',{id:'child'},[
-        React.createElement('h1',{},"I'm a h1 tag"),
-        React.createElement('h2',{},"I'm a h2 tag")
-    ]
-    )],[
-        React.createElement('div',{id:'child'},[
-            React.createElement('h1',{},"I'm a h1 tag"),
-            React.createElement('h2',{},"I'm a h2 tag")
-        ]
-        )])
-        console.log(children) //
-         const root = ReactDOM.createRoot(document.getElementById('root'))
-         root.render(children)
+const jsxheading =<h1>Hello from JSX</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(jsxheading)
