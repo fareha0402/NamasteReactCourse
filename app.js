132bee4369842d5ae0b15b4dba281e03bb5fea27
unknown <fareha0402@gmail.com>
2024-09-02 16:56:25 +0530

    Ch-3 :jsx vs html:css

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const jsxheading =<h1>Hello from JSX</h1>
+const jsxheading =(<h1 id="heading" className="head">
+    Hello from JSX
+    </h1>)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(jsxheading)
+
+// React functional component
