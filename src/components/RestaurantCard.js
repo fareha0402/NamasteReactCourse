41dbbb1377611d72727e4308a023378364670066
unknown <fareha0402@gmail.com>
2024-09-20 16:54:26 +0530

    Config driven UI and destructuring props

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,15 +1,16 @@
 const RestaurantCard = (props) =>{
-    const resItem = props.resData.info
-    console.log(resItem)
-    const imgSrc= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resItem.cloudinaryImageId
+    const {resData} = props // Destructuring
+    // const resItem = props.resData.info
+    console.log(resData.info)
+    const imgSrc= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.info.cloudinaryImageId
     return(
         <div className="resCard">
             <img className="resLogo" src={imgSrc}></img>
-            <h3>{resItem.name}</h3>
-            <h4>{resItem.avgRating} stars</h4>
-            <h4>{resItem.areaName}</h4>
-            <h4>{resItem.cuisines.join(',')}</h4>
-            <h4>{resItem.sla.slaString}</h4>
+            <h3>{resData.info.name}</h3>
+            <h4>{resData.info.avgRating} stars</h4>
+            <h4>{resData.info.areaName}</h4>
+            <h4>{resData.info.cuisines.join(',')}</h4>
+            <h4>{resData.info.sla.slaString}</h4>
         </div>
     )
 }
