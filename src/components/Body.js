67e866bdadaa9a2ae79e5f6c3ff5d4471b3bfadc
unknown <fareha0402@gmail.com>
2024-09-22 17:48:34 +0530

    Hooks,Diff algo, React useState, destructuring

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,23 @@
 import RestaurantCard from "./RestaurantCard"
 import restaurantsList from '../Utils/restaurants.json';
+import { useState } from "react";
 const Body = () => {
-    const list = restaurantsList
-        console.log(list)
+    //Local Stateful variable,Superpowerful Variable
+    const [listofRestaurants,setListofRestaurants] = useState(restaurantsList)
+    
+    // let listofRestaurants = restaurantsList // Normal JS variable
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            {/* <div className="search">Search</div> */}
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const filteredList = listofRestaurants.filter(res => res.info.avgRating > 4.5)
+                    setListofRestaurants(filteredList)
+                }}>Top Rated restaurant</button>
+            </div>
             <div className="res-Container">
-               { list.map((restaurant) => (
+               { listofRestaurants.map((restaurant) => (
                     <RestaurantCard key={restaurant.id} resData={restaurant}/>
                 ))}
             </div>
